85ce681ad8726e9aff1e01f6ed5a486e212473b1
Guilherme Vieira <guilherme.camara@edu.pucrs.br>
2023-05-02 00:18:53 -0300

    implementado circuit breaker em CurrencyExchangeService

--- currency-exchange-service/src/main/java/com/engsoft2/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/engsoft2/currencyexchangeservice/CurrencyExchangeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 public class CurrencyExchangeController {
     private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
@@ -17,14 +19,21 @@ public class CurrencyExchangeController {
     private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @CircuitBreaker(name = "CECircuitBreaker", fallbackMethod = "retrieveFallback")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
         if(currencyExchange ==null) {
             throw new RuntimeException("Unable to Find data for " + from + " to " + to);
         }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
         return currencyExchange;
+    }
+
+    private RuntimeException retrieveFallback(@PathVariable String from, @PathVariable String to){
+        logger.info("Circuit breaker em acao");
+        return new RuntimeException("Unable to Find data for " + from + " to " + to);
     }
 }
