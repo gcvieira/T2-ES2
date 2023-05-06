9af8b7ee5c8607336e13fc1d313afc2944c30008
Guilherme Vieira <guilherme.camara@edu.pucrs.br>
2023-05-05 22:47:14 -0300

    circuit breaker implementado em CurrencyConversionController

--- currency-conversion-service/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionController.java
@@ -4,22 +4,57 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import java.util.function.Supplier;
 
 @RestController
 public class CurrencyConversionController {
     @Autowired
     private CurrencyExchangeProxy proxy;
+	private CircuitBreakerFactory circuitBreakerFactory;
+
+    public CurrencyConversionController(CircuitBreakerFactory cbFactory) {
+        this.circuitBreakerFactory = cbFactory;
+    }
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+    public CurrencyConversion calculateCurrencyConversionController(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
         HashMap<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("from",from);
 		uriVariables.put("to",to);
+
+		return circuitBreakerFactory.create("calculateCurrencyConversion").run((java.util.function.Supplier<CurrencyConversion>) this.calculateCurrencyConversionSupplier(from, to, quantity), (Throwable t) -> {
+            CurrencyConversion fallback = new CurrencyConversion(
+                Long.valueOf(1), 
+                from, to, BigDecimal.valueOf(1), 
+                BigDecimal.valueOf(1), 
+                BigDecimal.valueOf(1), 
+                "rest template"
+            );
+			return fallback;
+		});
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeignController(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+		return circuitBreakerFactory.create("calculateCurrencyConversionFeign").run((java.util.function.Supplier<CurrencyConversion>) this.calculateCurrencyConversionFeignSupplier(from, to, quantity), t -> {
+            CurrencyConversion fallback = new CurrencyConversion(
+                Long.valueOf(1), 
+                from, to, BigDecimal.valueOf(1), 
+                BigDecimal.valueOf(1), 
+                BigDecimal.valueOf(1), 
+                "feign"
+            );
+			return fallback;
+		});
+    }
+
+	public CurrencyConversion calculateCurrencyConversion(String from, String to, BigDecimal quantity) {
         ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
         CurrencyConversion currencyConversion = responseEntity.getBody();
         return new CurrencyConversion(
@@ -29,10 +64,13 @@ public class CurrencyConversionController {
 			quantity.multiply(currencyConversion.getConversionMultiple()), 
 			currencyConversion.getEnvironment()+ " " + "rest template"
         );
-    }
+	}
 
-    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+	public Supplier<CurrencyConversion> calculateCurrencyConversionSupplier(String from, String to, BigDecimal quantity) {
+		return () -> this.calculateCurrencyConversion(from, to, quantity);
+	}
+
+	public CurrencyConversion calculateCurrencyConversionFeign(String from, String to, BigDecimal quantity) {
         CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
         return new CurrencyConversion(currencyConversion.getId(), 
 			from, to, quantity, 
@@ -40,5 +78,9 @@ public class CurrencyConversionController {
 			quantity.multiply(currencyConversion.getConversionMultiple()), 
 			currencyConversion.getEnvironment() + " " + "feign"
         );
-    }
+	}
+
+	public Supplier<CurrencyConversion> calculateCurrencyConversionFeignSupplier(String from, String to, BigDecimal quantity) {
+		return () -> this.calculateCurrencyConversionFeign(from, to, quantity);
+	}
 }
--- currency-conversion-service/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,8 +1,17 @@
 package com.engsoft2.currencyconversionservice;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +21,12 @@ public class CurrencyConversionServiceApplication {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
 
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory
+			.configureDefault(id -> new Resilience4JConfigBuilder(id)
+			.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build())
+			.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+			.build());
+	}
 }
